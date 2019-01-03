af23b060b5d9d0368b4627523dcf8adda23cf257
bpohlmann <benni20001025googlemail.com>
2019-01-03 14:08:29 +0100

    1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+	
+	public Vector3 offset;
+	
+	public bool useOffsetValues;
+	
+	public float rotateSpeed;
+	
+	public Transform pivot;
+	
+	public float maxViewAmgle;
+	public float minViewAmgle;
+	
+	public bool invertY;
+	
+	// Use this for initialization
+	void Start () {
+		if (!useOffsetValues){
+			offset = target.position - transform.position;
+		}
+		
+		pivot.transform.position = target.transform.position;
+		//pivot.transform.parent = target.transform;
+		
+		pivot.transform.parent = null;
+		Cursor.lockState = CursorLockMode.Locked;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		
+		pivot.transform.position = target.transform.position;
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		pivot.Rotate(0, horizontal, 0);
+		
+		
+		 
+		float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //pivot.Rotate(vertical, 0 , 0);
+		if (invertY){
+			pivot.Rotate(vertical, 0 , 0);
+		}
+		else {
+			pivot.Rotate(-vertical, 0 , 0);
+		}
+		if (pivot.rotation.eulerAngles.x > maxViewAmgle && pivot.rotation.eulerAngles.x < 180f){
+			pivot.rotation = Quaternion.Euler(maxViewAmgle, 0, 0);
+		}
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAmgle){
+			pivot.rotation = Quaternion.Euler(360f + minViewAmgle, 0, 0);
+		}
+		float desireYAngle = pivot.eulerAngles.y;
+		float desireXAngle = pivot.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler(desireXAngle, desireYAngle, 0);
+		
+		
+		transform.position = target.position - (rotation *offset);
+		
+		if(transform.position.y < target.position.y){
+			transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+		}
+		
+		transform.LookAt(target);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+	public float moveSpeed;
+	//public Rigidbody therB;
+	public float jumpForce;
+	public CharacterController controller;
+	private Vector3 moveDirection;
+	public float gravityScale;
+	
+	public Animator anim;
+	public Transform pivot;
+	public float rotateSpeed;
+	
+	public GameObject playerModel;
+	// Use this for initialization
+	void Start () {
+		//therB = GetComponent<Rigidbody>();
+		controller = GetComponent<CharacterController>();
+	}
+	  
+    
+	// Update is called once per frame
+	void Update () {
+		/*therB.velocity = new Vector3(Input.GetAxis("Horizontal")* moveSpeed, therB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+		if (Input.GetButtonDown("Jump"))
+		{
+			therB.velocity = new Vector3(therB.velocity.x, jumpForce, therB.velocity.z);
+		} */
+		
+		
+		//moveDirection= new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+		float yStore = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis("Vertical") ) + (transform.right * Input.GetAxis("Horizontal") );
+		moveDirection = moveDirection.normalized * moveSpeed;
+		moveDirection.y = yStore;
+		if (controller.isGrounded)
+		{
+			moveDirection.y = 0f;
+		  if (Input.GetButtonDown("Jump"))
+		  {
+			  moveDirection.y = jumpForce;
+		  }
+		}
+		
+		
+		moveDirection.y = moveDirection.y + (Physics.gravity.y  * gravityScale * Time.deltaTime);
+		controller.Move(moveDirection * Time.deltaTime);
+		
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
+			transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+			Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+			playerModel.rotation = Quaternion.Slerp(playerModel.transform, newRotation, rotateSpeed * Time.deltaTime);
+		}
+		
+		
+		
+		anim.SetBool("isGrounded", controller.isGrounded);
+		anim.SetFloat("speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
+	}
+}
